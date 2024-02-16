3b1d122d703abd5dce1460be673888ca88b45f4c
Oscar Martin <oscar.martin@tietoevry.com>
2024-02-16 11:45:58 +0100

    Inital commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const port = 5000;
+const jose = require("node-jose");
+const ms = require("ms");
+
+const keyStore = jose.JWK.createKeyStore();
+
+keyStore.generate("RSA", 2048, { alg: "RS256", use: "sig" }).then((result) => {
+  fs.writeFileSync(
+    "Keys.json",
+    JSON.stringify(keyStore.toJSON(true), null, "  ")
+  );
+});
+
+app.use(express.json());
+app.get("/jwks", async (req, res) => {
+  const ks = fs.readFileSync("keys.json");
+
+  const keyStore = await jose.JWK.asKeyStore(ks.toString());
+
+  res.send(keyStore.toJSON());
+});
+
+app.post("/tokens", async (req, res) => {
+  const incPayload = req.body;
+
+  const JWKeys = fs.readFileSync("keys.json");
+
+  const keyStore = await jose.JWK.asKeyStore(JWKeys.toString());
+
+  const [key] = keyStore.all({ use: "sig" });
+
+  const opt = { compact: true, jwk: key, fields: { typ: "jwt" } };
+
+  const payload = JSON.stringify({
+    ...incPayload,
+    exp: Math.floor((Date.now() + ms("100d")) / 1000),
+    iat: Math.floor(Date.now() / 1000),
+  });
+
+  const token = await jose.JWS.createSign(opt, key).update(payload).final();
+  res.send({ token });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
